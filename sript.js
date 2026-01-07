9ece2fc9f213cfdb8a0d73261f1dc5ce287964f9
Chathuri Wasundara <chathuriw222@gmail.com>
2026-01-07 22:07:31 +0530

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,54 @@
+// pizza
+const pizzaItem = document.getElementById("pizza");
+
+pizzaItem.addEventListener("mouseover", function () {
+  pizzaItem.style.fontSize = "24px";
+});
+
+pizzaItem.addEventListener("mouseout", function () {
+  pizzaItem.style.fontSize = "";
+});
+
+// burger
+const burgerItem = document.getElementById("burger");
+
+burgerItem.addEventListener("mouseover", function () {
+  burgerItem.style.fontSize = "24px";
+});
+
+burgerItem.addEventListener("mouseout", function () {
+  burgerItem.style.fontSize = "";
+});
+
+// chips
+const chips = document.getElementById("chips");
+
+chips.addEventListener("mouseover", function () {
+    chips.style.fontSize = "24px";
+});
+
+chips.addEventListener("mouseout", function () {
+    chips.style.fontSize = "";
+});
+
+// donuts
+const donuts = document.getElementById("donuts");
+
+donuts.addEventListener("mouseover", function () {
+    donuts.style.fontSize = "24px";
+});
+
+donuts.addEventListener("mouseout", function () {
+    donuts.style.fontSize = "";
+});
+
+// drinks
+const drinks = document.getElementById("drinks");
+
+drinks.addEventListener("mouseover", function () {
+    drinks.style.fontSize = "24px";
+});
+
+drinks.addEventListener("mouseout", function () {
+    drinks.style.fontSize = "";
+});
